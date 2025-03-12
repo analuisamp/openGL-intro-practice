e4358f4b9e46b0f75a86bf5d5e1d428be891207e
ana luisa miranda <analuisamirandap@gmail.com>
2025-03-12 16:18:02 -0300

    atv2

--- cube.c
+++ cube.c
@@ -52,22 +52,20 @@ void init(void)
 
 void display(void)
 {
-   glClear (GL_COLOR_BUFFER_BIT);
-   glColor3f (1.0, 1.0, 1.0);
-   glLoadIdentity ();             /* clear the matrix */
-           /* viewing transformation  */
-   gluLookAt (0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-   glScalef (1.0, 2.0, 1.0);      /* modeling transformation */ 
-   glutWireCube (1.0);
-   glFlush ();
+   glClear(GL_COLOR_BUFFER_BIT);
+   glLoadIdentity();
+   glTranslatef(0.0, 0.0, -5.0);
+   glScalef(1.0, 2.0, 1.0);
+   glutWireCube(1.0);
+   glFlush();
 }
 
 void reshape (int w, int h)
 {
-   glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
    glMatrixMode (GL_PROJECTION);
    glLoadIdentity ();
-   glFrustum (-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
+   glOrtho (-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
    glMatrixMode (GL_MODELVIEW);
 }
 
